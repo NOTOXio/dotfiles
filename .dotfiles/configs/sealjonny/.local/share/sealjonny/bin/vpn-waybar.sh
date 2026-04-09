bcdcb9945d095e51d5376fec447497996a09b1da
Ivo Brauns <ivo.jonathan.brauns@gmail.com>
2026-04-09 14:01:43 +0200

    Adjusted to my configs

--- .dotfiles/configs/sealjonny/.local/share/sealjonny/bin/vpn-waybar.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if nmcli -g TYPE connection show --active | grep -x "wireguard" &> /dev/null; then
-  "$HOME/.local/share/sealjonny/bin/vpn" disconnect
-else
-  exec "$TERMINAL" --class=CustomVPN -e env VPN_FZF_HEIGHT=100% "$HOME/.local/share/sealjonny/bin/vpn" connect
-fi
